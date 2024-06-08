ee2f59ef33186270890d0f3f3c8572e4ac0929eb
LeoL10 <S11245@kcis.com.tw>
2024-06-08 22:17:48 +0800

    Player movement complete

--- wesleo/Assets/Scripts/PlayerController.cs
+++ wesleo/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,20 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode throwBall;
+
     private Rigidbody2D theRB;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+
+    public bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +31,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if(Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        } 
+        else if(Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        } 
+        else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+        if(Input.GetKeyDown(jump) && isGrounded)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
     }
 }
